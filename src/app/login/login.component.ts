680e75620a9edff2a71422300fe083b0b964c5d3
MohitClerisy <147398480+MohitClerisy@users.noreply.github.com>
2024-02-01 19:43:08 +0530

    Revert "removed comments and unused consoles"

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,6 +30,9 @@ export class LoginComponent implements OnInit {
         "",
         [
           Validators.required,
+          // Validators.pattern(
+          //   "(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!#^~%*?&,.<>\"'\\;:{\\}\\[\\]\\|\\+\\-\\=\\_\\)\\(\\)\\`\\/\\\\\\]])[A-Za-z0-9d$@].{7,}"
+          // )
         ]
       ]
     });
@@ -40,6 +43,7 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin(): void {
+    // console.log(this.loginForm.value);
     this.submitted = true;
     if (this.loginForm.valid) {
       const payload = this.loginForm.getRawValue();
